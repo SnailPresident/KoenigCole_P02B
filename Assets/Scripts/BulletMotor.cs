b9ff36c363f6792e5d366ffc3331db2d541b2489
Cole <cole.koenig08@gmail.com>
2020-10-24 20:08:07 -0500

    Added missiles to enemies

---
+++ Assets/Scripts/BulletMotor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class BulletMotor : MonoBehaviour
+{
+    [SerializeField] float _bulletSpeed = 10f;
+    [SerializeField] float _countdownTime = 10f;
+    [SerializeField] float _effectTime = 0.2f;
+
+    public Vector3 _velocity = Vector3.zero;
+    private Rigidbody _rb = null;
+
+    Coroutine _destroyCountdown, _destroyEffects;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _destroyCountdown = StartCoroutine(DestroyIfNotCollided(_countdownTime));
+    }
+
+    private void FixedUpdate()
+    {
+        MoveForward();
+        _velocity = _rb.velocity;
+    }
+
+    private void MoveForward()
+    {
+        _rb.AddRelativeForce(Vector3.forward * _bulletSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _destroyEffects = StartCoroutine(DestroySelf(_effectTime));
+    }
+
+    IEnumerator DestroyIfNotCollided(float secondsTillDestroy)
+    {
+        yield return new WaitForSeconds(secondsTillDestroy);
+        Destroy(gameObject);
+    }
+
+    IEnumerator DestroySelf(float secondsToWait)
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemyFire.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(EnemyScript))]
+
+public class EnemyFire : MonoBehaviour
+{
+    [SerializeField] GameObject _projectile = null;
+    [SerializeField] Transform _shotOrigin = null;
+    [SerializeField] float _firingDelaySeconds = 3f;
+    private EnemyScript enemyScript = null;
+
+    [SerializeField] ParticleSystem _shotParticle = null;
+    [SerializeField] AudioClip _shotAudio = null;
+
+    Coroutine _firingCoroutine = null;
+
+    private void Awake()
+    {
+        enemyScript = GetComponent<EnemyScript>();
+    }
+
+    private void OnEnable()
+    {
+        enemyScript.TargetFound += OnTargetFound;
+        enemyScript.TargetLost += OnTargetLost;
+    }
+
+    private void OnDisable()
+    {
+        enemyScript.TargetFound -= OnTargetFound;
+        enemyScript.TargetLost -= OnTargetLost;
+    }
+
+    private void OnTargetFound()
+    {
+        if (_firingCoroutine == null)
+        {
+            _firingCoroutine = StartCoroutine(FiringDelay(_firingDelaySeconds));
+        }
+    }
+
+    private void OnTargetLost()
+    {
+        if (_firingCoroutine != null)
+        {
+            StopCoroutine(_firingCoroutine);
+            _firingCoroutine = null;
+        }
+    }
+
+    IEnumerator FiringDelay(float delayInSeconds)
+    {
+        FireShot();
+        yield return new WaitForSeconds(delayInSeconds);
+        _firingCoroutine = null;
+    }
+
+    private void FireShot()
+    {
+        Debug.Log("Firing!");
+        Instantiate(_projectile, _shotOrigin.transform.position, _shotOrigin.transform.rotation) ;
+        PlayShotSound();
+        PlayShotParticle();
+    }
+
+    public void PlayShotSound()
+    {
+        //AudioHelper.PlayClip2D(_shotAudio, .25f);
+    }
+
+    public void PlayShotParticle()
+    {
+       // _shotParticle.Play();
+    }
+}
