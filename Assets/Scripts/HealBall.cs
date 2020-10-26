b61bf0d2146ebb8ed4faa65412805b6021eaaae6
Cole <cole.koenig08@gmail.com>
2020-10-26 13:18:32 -0500

    Added Health pickups

---
+++ Assets/Scripts/HealBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealBall : MonoBehaviour
+{
+    [SerializeField] AudioClip HealSound = null;
+    public float healAmount = 6;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerMovement health = other.gameObject.GetComponent<PlayerMovement>();
+        AudioHelper.PlayClip2D(HealSound, .25f);
+        health.playerHealth += healAmount;
+        this.gameObject.SetActive(false);
+    }
+}
